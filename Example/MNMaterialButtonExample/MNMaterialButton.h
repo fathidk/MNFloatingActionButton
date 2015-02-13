7e52b619a7606e2ebeac5eb9b0edd9314765e249
Matthew Nydam <mattnydam@Matthews-MacBook-Pro.local>
2015-02-13 15:33:13 +1300

    Added podspec, License etc.

---
+++ Example/MNMaterialButtonExample/MNMaterialButton.h
@@ -0,0 +1,36 @@
+//
+//  MNMaterialButton.h
+//  Matt Nydam
+//
+//  Created by Matthew Nydam on 1/12/14.
+//  Copyright (c) 2014 MN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//IB_DESIGNABLE. Soon http://i3.kym-cdn.com/entries/icons/original/000/005/673/soon_(1).jpg
+IB_DESIGNABLE
+@interface MNMaterialButton : UIControl
+
+// -- Appearance
+
+// Defaults to plus symbol. Set to nil to remove image completely.
+@property (nonatomic, strong) IBInspectable UIImageView *centerImageView;
+@property (nonatomic, strong) IBInspectable UIColor *backgroundColor;
+
+// Defaults to background color because I was inspired by this http://ianstormtaylor.com/design-tip-never-use-black/
+@property (nonatomic, strong) IBInspectable UIColor *shadowColor;
+// Default = 0.6
+@property (nonatomic) IBInspectable CGFloat  shadowOpacity;
+// Default = 1.5
+@property (nonatomic) IBInspectable CGFloat  shadowRadius;
+
+// -- Animation
+
+// Default = 0.95
+@property (nonatomic) CGFloat animationScale;
+// Default = 0.05f
+@property (nonatomic) CGFloat animationDuration;
+
+@end
---
+++ MNMaterialButton.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+  s.name             = "MNMaterialButton"
+  s.version          = "0.1.1"
+  s.summary          = "A F.A.B for iOS"
+  s.description      = "A customisable FAB (Floating Action Button)"
+  s.homepage         = "http://matthewnydam.com/"
+  s.license          = 'MIT'
+  s.author           = { "Matt Nydam" => "matt@matthewnydam.com" }
+  s.source           = { :git => "https://github.com/mattnydam/MNMaterialButton.git", :tag => s.version.to_s }
+
+  s.platform     = :ios, '7.0'
+  s.requires_arc = true
+
+  s.source_files = 'MNMaterialButton'
+
+end
